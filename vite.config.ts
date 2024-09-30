c017e3f4258c7b06d1b639be79f6712f3012405f
Erik <erikhau3010@gmail.com>
2024-10-01 01:26:34 +0700

    chore: update vite.config.ts to support multiple output formats

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     lib: {
       entry: "src/index.ts",
       name: "BarChartRace",
+      formats: ["es", "umd", "cjs"],
       fileName: (format) => `bar-chart-race.${format}.js`,
     },
   },
