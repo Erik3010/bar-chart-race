a5341e0686696b47686901ae7fb3a939503e672b
Erik <erikhau3010@gmail.com>
2025-07-11 14:42:38 +0800

    feat: enhance BarChart with interpolation and timeline interaction

--- src/BarChart.ts
+++ src/BarChart.ts
@@ -17,7 +17,8 @@ class BarChart {
   private height: number;
   private data: DataType[];
 
-  private dateIndex = -1;
+  private currentPercentage = 0;
+  private isAnimating = false;
 
   private padding: { top: number; right: number; bottom: number; left: number };
   private bars: Bar[];
@@ -35,12 +36,60 @@ class BarChart {
     this.padding = { top: 30, right: 30, bottom: 60, left: 100 };
     this.bars = [];
   }
+
   getDataByDate(date: Dataset["date"] | null) {
     return this.data.map(({ label, datasets }) => ({
       label,
       value: datasets.find((data: Dataset) => data.date === date)?.value ?? 0,
     }));
   }
+
+  // New method to get interpolated data based on percentage
+  getInterpolatedData(percentage: number) {
+    const maxIndex = this.dateLabels.length - 1;
+    const exactIndex = percentage * maxIndex;
+    const lowerIndex = Math.floor(exactIndex);
+    const upperIndex = Math.ceil(exactIndex);
+
+    // If we're exactly on a data point, return that data
+    if (lowerIndex === upperIndex) {
+      return this.getDataByDate(this.dateLabels[lowerIndex]);
+    }
+
+    // Interpolate between two data points
+    const lowerData = this.getDataByDate(this.dateLabels[lowerIndex]);
+    const upperData = this.getDataByDate(this.dateLabels[upperIndex]);
+    const interpolationFactor = exactIndex - lowerIndex;
+
+    return this.data.map(({ label }) => {
+      const lowerValue = lowerData.find((d) => d.label === label)?.value ?? 0;
+      const upperValue = upperData.find((d) => d.label === label)?.value ?? 0;
+      const interpolatedValue =
+        lowerValue + (upperValue - lowerValue) * interpolationFactor;
+
+      return {
+        label,
+        value: interpolatedValue,
+      };
+    });
+  }
+
+  // Get current interpolated date string
+  getCurrentInterpolatedDate(): string {
+    const maxIndex = this.dateLabels.length - 1;
+    const exactIndex = this.currentPercentage * maxIndex;
+    const lowerIndex = Math.floor(exactIndex);
+    const upperIndex = Math.ceil(exactIndex);
+
+    if (lowerIndex === upperIndex) {
+      return this.dateLabels[lowerIndex];
+    }
+
+    // For interpolated dates, we'll show the lower date
+    // You could implement more sophisticated date interpolation here
+    return this.dateLabels[lowerIndex];
+  }
+
   getLargestDataByDate(date: Dataset["date"] | null) {
     const data = this.getDataByDate(date);
     return data.reduce(
@@ -48,36 +97,29 @@ class BarChart {
       data[0]
     );
   }
+
+  // Get largest value from interpolated data
+  getLargestInterpolatedData(percentage: number) {
+    const data = this.getInterpolatedData(percentage);
+    return data.reduce(
+      (acc, cur) => (acc.value > cur.value ? acc : cur),
+      data[0]
+    );
+  }
+
   get dateLabels() {
     if (this.data.length === 0) return [];
     return this.data[0].datasets.map((data) => data.date);
   }
-  get currentDate() {
-    if (this.dateIndex < 0) return null;
-    return this.dateLabels[this.dateIndex];
-  }
+
   get currentData() {
-    return this.getDataByDate(this.currentDate);
+    return this.getInterpolatedData(this.currentPercentage);
   }
+
   get largestCurrentData() {
-    return this.getLargestDataByDate(this.currentDate);
+    return this.getLargestInterpolatedData(this.currentPercentage);
   }
-  get nextDate() {
-    const nextIndex = this.dateIndex + 1;
-    if (nextIndex >= this.dateLabels.length) return null;
-    return this.dateLabels[nextIndex];
-  }
-  get nextData() {
-    if (!this.nextDate) return null;
-    return this.getDataByDate(this.nextDate);
-  }
-  get largestNextData() {
-    if (!this.nextDate) return null;
-    return this.getLargestDataByDate(this.nextDate);
-  }
-  get lastDate() {
-    return this.dateLabels[this.dateLabels.length - 1];
-  }
+
   get chartDimension(): Dimension {
     return {
       x: this.padding.left,
@@ -86,47 +128,58 @@ class BarChart {
       height: this.height - this.padding.top - this.padding.bottom,
     };
   }
+
   get barHeight() {
     return this.chartDimension.height / this.data.length;
   }
-  get isInitialAnimation() {
-    return this.dateIndex === -1;
-  }
+
   init() {
     this.containerElement.appendChild(this.canvas);
 
     this.initBars();
     this.initTimelineBar();
     this.render();
-    this.startAnimation();
+
+    // Start at 0% (first data point)
+    this.setPercentage(0);
   }
-  async startAnimation() {
-    while (this.nextDate) {
-      const animations = this.bars.map((bar) => {
-        const nextBarData = this.nextData?.find(
-          (data) => data.label === bar.label
-        );
 
-        const largestNextData = Math.max(this.largestNextData!.value, 1);
-        const widthRatio = nextBarData!.value / largestNextData;
-        const newBarWidth = this.chartDimension.width * widthRatio;
-
-        return bar.animateTo(
-          newBarWidth,
-          nextBarData!.value,
-          this.isInitialAnimation ? 1500 : 2500
-        );
-      });
-
-      const timelineAnimations = !this.isInitialAnimation
-        ? [this.timelineBar.animateTo(this.dateIndex + 1)]
-        : [];
-      await Promise.all([...animations, ...timelineAnimations]);
-      await sleep(100);
-
-      this.dateIndex++;
+  private async setPercentage(newPercentage: number) {
+    if (newPercentage === this.currentPercentage || this.isAnimating) {
+      return;
     }
+
+    this.isAnimating = true;
+    this.currentPercentage = newPercentage;
+
+    // Get the interpolated data for the new percentage
+    const newData = this.getInterpolatedData(this.currentPercentage);
+    const largestNewData = Math.max(
+      this.getLargestInterpolatedData(this.currentPercentage).value,
+      1
+    );
+
+    // Animate bars to new positions
+    const animations = this.bars.map((bar) => {
+      const newBarData = newData.find((data) => data.label === bar.label);
+      const widthRatio = newBarData!.value / largestNewData;
+      const newBarWidth = this.chartDimension.width * widthRatio;
+
+      return bar.animateTo(
+        newBarWidth,
+        newBarData!.value,
+        200 // Short animation for smooth dragging
+      );
+    });
+
+    await Promise.all(animations);
+    this.isAnimating = false;
   }
+
+  private handleTimelineChange(percentage: number) {
+    this.setPercentage(percentage);
+  }
+
   sortBarChart() {
     for (let i = 0; i < this.bars.length; i++) {
       const currentBar = this.bars[i];
@@ -145,6 +198,7 @@ class BarChart {
       }
     }
   }
+
   initBars() {
     const datum = this.currentData;
     for (const [index, data] of datum.entries()) {
@@ -152,6 +206,7 @@ class BarChart {
       this.bars.push(bar);
     }
   }
+
   initTimelineBar() {
     const bottomOffset = 25;
     const coordinateY = this.height - this.padding.bottom + bottomOffset;
@@ -161,8 +216,10 @@ class BarChart {
       start: { x: this.padding.left, y: coordinateY },
       end: { x: this.width - this.padding.right, y: coordinateY },
       labels: this.dateLabels,
+      onPercentageChange: this.handleTimelineChange.bind(this),
     });
   }
+
   createBar(data: { label: string; value: number }, index: number) {
     const { x, y, width } = this.chartDimension;
 
@@ -183,16 +240,15 @@ class BarChart {
       value: data.value,
     });
   }
-  drawLabels() {
-    if (this.isInitialAnimation) return;
 
+  drawLabels() {
     const total = this.bars.reduce((acc, bar) => acc + bar.value, 0);
     const labelX = this.width - this.padding.right;
     const labelY = this.height - this.padding.bottom;
     const bottomOffset = 32;
 
     this.drawCounterLabel({
-      text: this.currentDate || "",
+      text: this.getCurrentInterpolatedDate(),
       font: "bold 64px Arial",
       x: labelX,
       y: labelY - bottomOffset,
@@ -207,6 +263,7 @@ class BarChart {
       color: "#ccc",
     });
   }
+
   drawCounterLabel(props: {
     text: string;
     font: string;
@@ -223,6 +280,7 @@ class BarChart {
     this.ctx.fillText(text, x, y);
     this.ctx.restore();
   }
+
   draw() {
     this.sortBarChart();
     this.drawLabels();
@@ -231,6 +289,7 @@ class BarChart {
     }
     this.timelineBar.draw();
   }
+
   render() {
     this.ctx.clearRect(0, 0, this.width, this.height);
     this.draw();
